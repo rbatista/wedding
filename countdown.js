be3c7f35df8252428125fd751116cd5d7d740b27
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2015-07-07 05:11:33 +0000

    Add guest confirmation controller.

--- countdown.js
+++ countdown.js
@@ -1,14 +1,25 @@
-angular.module('weddingApp', [])
-	.controller('CountdownController', function () {
-		var countdown = this;
+(function () {
+	'use strict';
 
-		countdown.getRemainingDays = function() {
+	angular
+		.module('weddingApp')
+		.controller('CountdownController', countdownController); 
+
+
+	function countdownController() {
+		var vm = this;
+
+		vm.getRemainingDays = getRemainingDays;
+
+		function getRemainingDays() {
 			var oneDay = 1000*60*60*24;
 			var today = new Date();
 			today.setHours(0, 0, 0, 0);
 			var theDay = new Date(2015, 10 - 1, 10, 0, 0, 0, 0);
 
 			return Math.abs((theDay - today) / (oneDay));
-		};
+		}
 
-	});
+	}
+
+})();
